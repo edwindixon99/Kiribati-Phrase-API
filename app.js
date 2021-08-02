1d414d0db9754e71cdd0532724645acbf37ecb12
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-08-03 00:19:39 +1200

    working connection to database

--- app.js
+++ app.js
@@ -1,70 +1,83 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 var mysql = require('mysql');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-module.exports = app;
+// module.exports = app;
 
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+  connectionLimit: 50,
   host:'localhost',
   user: "root",
   password: '',
   database: 'Kiribati'
 });
 
-connection.connect(function(error) {
-  if (!!error) {
-    console.log('error');
-  }else {
-    console.log('fone');
-  }
-});
+// connection.connect(function(error) {
+//   if (!!error) {
+//     console.log('error');
+//   } else {
+//     console.log('fone');
+//   }
+// });
 
 app.get('/', function(req, res) {
-  connection.query("select * from null_Table", function(error, rows, fields) {
+  connection.getConnection(function(error, tempCont) {
     if (!!error) {
       console.log('error');
     }else {
       console.log('done');
+      tempCont.query("select * from null_Table", function(error, rows, fields) {
+        tempCont.release();
+        if(!!error) {
+          console.log('Error in the query');
+        } else {
+          res.json(rows)
+        }
+    
+      });
     }
+
   });
+    
+  
 })
 
 app.listen(1337);

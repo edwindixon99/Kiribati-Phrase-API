740f54d483532995f6651e3fb44425226ee2adae
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-09-20 03:30:27 +1200

    added users db

---
+++ app/middleware/user-middleware.js
@@ -0,0 +1,23 @@
+const {OAuth2Client} = require('google-auth-library');
+
+// https://developers.google.com/identity/sign-in/web/backend-auth
+
+
+
+
+exports.googleVerify = async function(CLIENT_ID) {
+    const client = new OAuth2Client(CLIENT_ID);
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        // const domain = payload['hd'];
+      }
+    verify().catch(console.error);
+}
